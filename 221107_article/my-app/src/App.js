7e52c8419d21804c1ef9eb564ddd633dfb77b826
lhj9520 <gw9520@naver.com>
2022-11-14 19:12:03 +0900

    221109 서버-mysql 연결/회원가입/로그인/세션 구현

--- 221107_article/my-app/src/App.js
+++ 221107_article/my-app/src/App.js
@@ -1,27 +1,21 @@
-import React from "react";
 import axios from "axios";
+import React from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import "./App.css";
 
-/**
- * 로그인 세션 사용하기 위한
- * 서버랑 app이랑 도메인이 다르자나 그러면 쿠키 공유가 안됨
- * 그래서 도메인이 달라고 내 쿠키를 공유해주겠다고 설정
- * app과 서버 둘 다 세션 관련된 설정을 해줘야함
- */
+// 도메인 달라도 쿠키공유해줄게
 axios.defaults.withCredentials = true;
 
 function Join() {
   const navigation = useNavigate();
 
-  //useState는 항상 위
+  // useState는 항상위
   const [data, setData] = React.useState({
     id: "",
     pw: "",
   });
 
   const 데이터변경 = (event) => {
-    // alert(event.target.name);
     const name = event.target.name;
     const cloneData = { ...data };
     cloneData[name] = event.target.value;
@@ -30,28 +24,29 @@ function Join() {
 
   const 회원가입 = async () => {
     /**
-     * GET : 가져올때
-     * POST : 뭐를 만들때
+     * GET : 가져올떄
+     * POST : 뭐를 생성할떄
      */
     await axios({
       url: "http://localhost:4000/join",
       method: "POST",
-      data: data, //get은 params였음
+      data: data,
     })
       .then((res) => {
         const { code, message } = res.data;
+
+        console.log(message);
 
         if (code === "success") {
           alert(message);
           navigation("/login");
         }
-
-        // console.log(res); //data에 응답 메세지 옴
       })
       .catch((e) => {
-        console.log("join error", e);
+        console.log("join Error", e);
       });
-    //여기서 내가 입력한 값을 console.log로 확인해주세요
+
+    // 여기서 내가 입력한 값을 console.log로 확인해주세요
   };
 
   return (
@@ -84,25 +79,24 @@ function Login() {
     await axios({
       url: "http://localhost:4000/login",
       method: "POST",
-      data: data, //get은 params였음
+      data: data,
     })
       .then((res) => {
-        const { code, message } = res.data;
+        alert(res.data.message);
 
-        if (code === "success") {
-          alert(message);
-          //로그인 후 메인페이지 이동해야겠지?
+        if (res.data.code === "success") {
+          window.location.href = "/";
         }
       })
       .catch((e) => {
-        console.log("login error", e);
+        console.log("로그인에러", e);
       });
   };
 
   return (
     <div>
-      <input type="text" name="id" onChange={데이터변경} />
-      <input type="password" name="pw" onChange={데이터변경} />
+      <input name="id" onChange={데이터변경} />
+      <input name="pw" onChange={데이터변경} />
       <button type="button" onClick={로그인}>
         로그인
       </button>
@@ -114,14 +108,42 @@ function Main() {
   return <div>Main</div>;
 }
 
+/**
+ * 1. React Router DOM (완료)
+ * npm install react-router-dom
+ *
+ * 2. Context Provider (완료)
+ * 두개 설정해주세요
+ * https://to2.kr/d6P
+ */
 const StoreContext = React.createContext({});
 
 function App() {
+  const [loginUser, setLoginUser] = React.useState({});
+
+  const 세션정보가져오기 = async () => {
+    await axios({
+      url: "http://localhost:4000/user",
+    }).then((res) => {
+      console.log(res.data);
+
+      setLoginUser(res.data);
+    });
+  };
+
+  React.useEffect(() => {
+    세션정보가져오기();
+  }, []);
+
   return (
-    <StoreContext.Provider value={{}}>
+    <StoreContext.Provider
+      value={{
+        loginUser,
+      }}
+    >
       <Routes>
         <Route exact path="/" element={<Main />} />
-        <Route exact path="login" element={<Login />} />
+        <Route exact path="/login" element={<Login />} />
         <Route exact path="/join" element={<Join />} />
       </Routes>
     </StoreContext.Provider>
--- 221107_article/server/server.js
+++ 221107_article/server/server.js
@@ -1,27 +1,24 @@
 const express = require("express");
 const cors = require("cors");
+/**
+ * 세션 호출
+ */
 const session = require("express-session");
+/**
+ * DB 연결 수행 전라이브러리 호출
+ */
+const mysql = require("mysql2");
+const db = mysql.createPoolCluster();
+
 const app = express();
 const port = 4000;
-
-/**
- * 임시 DB 만들어줌
- * 다음시간에 Mysql 사용할거임
- */
-const DB = {
-  user: [
-    {
-      id: "asd",
-      pw: "asd",
-    },
-  ],
-};
 
 /**
  * post로 데이터 받으려면 이거 넣어야 됨
  * 안적으면 undefined 출력
  */
 app.use(express.json());
+
 /**
  * 로그인 세션 사용하려면 적어줭~
  */
@@ -32,6 +29,7 @@ app.use(
     saveUninitialized: true,
   })
 );
+
 app.use(
   cors({
     origin: true,
@@ -39,58 +37,122 @@ app.use(
   })
 );
 
-app.get("/", (req, res) => {
+/**
+ * DB 서버 연결 정보
+ */
+db.add("articleproject", {
+  host: "127.0.0.1",
+  user: "root",
+  password: "",
+  database: "aritlce_project", //아놔 오타 ㅋ
+  port: 3306,
+});
+
+function 디비실행(query) {
+  return new Promise(function (resolve, reject) {
+    db.getConnection("articleproject", function (error, connection) {
+      if (error) {
+        console.log("DB 연결 오류", error);
+        reject(true);
+      }
+
+      connection.query(query, function (error, data) {
+        if (error) {
+          console.log("쿼리 오류", error);
+          reject(true);
+        }
+        resolve(data);
+      });
+
+      //쿼리 밑에 안에가 아니고
+      connection.release();
+    });
+  });
+}
+app.get("/", async (req, res) => {
+  /**
+   * 비동기라서 클라이언트로 데이터를 못줌
+   * 동기로 바꿔야 됨
+   * async await 그리고 Promise!!
+   * 디비를 연결해서 쿼리를 사용해야할때마다 이렇게 연결해줘야함 -> 함수화 하여 사용(new Promise 부분 그대로 함수로 뺌)
+   */
+  const 데이터 = await 디비실행("select * from user");
+
+  console.log(데이터);
+
   res.send("안녕하슈");
 });
 
-app.post("/join", (req, res) => {
-  //post에서 확인하려면 body get은 query
-  //   console.log(req.body);
+app.post("/join", async (req, res) => {
   const { id, pw } = req.body;
-  //   console.log(id, pw);
 
-  DB.user.push({
-    id: id,
-    pw: pw,
-  });
-
-  console.log(DB.user);
-
-  res.send({
+  const result = {
     code: "success",
     message: "회원가입되었습니다",
-  });
+  };
+
+  /**
+   * 아이디 중복체크
+   * - DB user테이블에 진짜 이 아이디가 존재하는지 확인해야함 !!
+   * 구현해주세요
+   */
+  const 회원 = await 디비실행(`SELECT * FROM user WHERE id = '${id}' `);
+
+  if (회원.length > 0) {
+    result.code = "error";
+    result.message = "이미 같은 아이디로 회원가입 되어있습니다";
+    res.send(result);
+    return;
+  }
+
+  // Mysql user 테이블에 INSERT 해줘야함 !!
+  const query = `INSERT INTO user(id,password,nickname) VALUES('${id}','${pw}','지나가던나그네')`;
+  await 디비실행(query);
+
+  res.send(result);
+});
+
+app.get("/user", (req, res) => {
+  res.send(req.session.loginUser);
 });
 
 app.get("/test", (req, res) => {
   console.log(req.session);
+
   res.send("//");
 });
 
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
   const { id, pw } = req.body;
 
   /**
-   * DB에 저장된 로그인 정보와 입력받은 로그인 정보와 비교하여 일치하는지 확인
-   * find
-   * 로그인에 성공하면 세선에 findUser를 저장해야함
+   * 1. 아이디랑 비밀번호가 같은 데이터가 있는 확인 (Mysql DB)
+   * - 같은 데이터가 존재하면 session 저장
+   * - 없을 경우 [회원 정보가 존재하지 않습니다] 메시지 보내주기~
    */
-  const user = DB.user;
-  const findUser = user.find((item) => {
-    return item.id === id && item.pw === pw;
-  });
 
-  console.log(findUser);
-  /**
-   * 세션 저장
-   * loginUser => 아무거나 써도 됨 변수 객체 키 내가 설정ㅇㅇ
-   */
-  req.session.loginUser = findUser;
+  const result = {
+    code: "success",
+    message: "로그인 되었습니다",
+  };
+
+  const 회원 = await 디비실행(
+    `SELECT * FROM user WHERE id='${id}' AND password='${pw}'`
+  );
+
+  if (회원.length === 0) {
+    result.code = "error";
+    result.message = "회원 정보가 존재하지 않습니다";
+    res.send(result);
+    return;
+  }
+
+  req.session.loginUser = 회원[0];
   req.session.save();
 
-  res.send("/");
+  res.send(result);
 });
 
 app.listen(port, () => {
-  console.log("서버가 실행되었습니다.");
+  console.log("서버가 시작되었습니다");
 });
